da0d9049145de5e212446c3fd93d7c3ffad0ac27
nikhil_jung <nikhilbudhathoki0@gmail.com>
2023-12-19 17:21:34 +0545

    basic changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
     <div className="App">
       <h1>Hello React</h1>
       <h2>separating the components</h2>
+      <h3>creting the popcorn app with separation of components</h3>
     </div>
   );
 }
